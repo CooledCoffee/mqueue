af1cf40ddf3b3b40eda21080fec9eb12c7d7df17
CooledCoffee <CooledCoffee@gmail.com>
2014-03-27 16:12:03 +0800

    exports in mqueue package

--- src/mqueue/__init__.py
+++ src/mqueue/__init__.py
@@ -1,3 +1,21 @@
 # -*- coding: utf-8 -*-
 
 QUEUE = None
+
+from mqueue.decorators import Task, Cron, Delay
+from mqueue import crons, util
+
+MINUTELY = crons.MINUTELY
+HOURLY = crons.HOURLY
+DAILY = crons.DAILY
+WEEKLY = crons.WEEKLY
+MONTHLY = crons.MONTHLY
+MINUTES = crons.MINUTES
+HOURS = crons.HOURS
+DAYS = crons.DAYS
+
+Delay = Delay
+task = Task
+cron = Cron
+
+start = util.start

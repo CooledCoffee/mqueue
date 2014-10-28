eb9276fcd93c60668fac06f291bd42905120a92c
CooledCoffee <cooledcoffee@gmail.com>
2014-10-28 10:23:45 +0800

    mqueue version 0.3

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='mqueue',
-    version='0.2',
+    version='0.3',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     classifiers = [

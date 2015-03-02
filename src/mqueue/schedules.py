fa82873ad40f96862bd6835c1aace92731c97d8b
CooledCoffee <cooledcoffee@gmail.com>
2015-03-02 10:55:50 +0800

    rename schedules

--- src/mqueue/schedules.py
+++ src/mqueue/schedules.py
@@ -10,26 +10,34 @@ class Schedule(object):
 class CronSchedule(Schedule):
     '''
     >>> from datetime import datetime
-    >>> schedule = CronSchedule('*/5 * * * *')
-    >>> schedule.is_overdue(datetime(2000, 1, 1, 0, 6, 0), datetime(2000, 1, 1, 0, 0, 0))
+    >>> schedule = CronSchedule('0 * * * *')
+    
+    >>> str(schedule)
+    'CronSchedule(0 * * * *)'
+    
+    >>> schedule.is_overdue(datetime(2000, 1, 1, 1, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
     True
-    >>> schedule.is_overdue(datetime(2000, 1, 1, 0, 3, 0), datetime(2000, 1, 1, 0, 0, 0))
+    >>> schedule.is_overdue(datetime(2000, 1, 1, 0, 30, 0), datetime(2000, 1, 1, 0, 0, 0))
     False
     '''
     
     def __init__(self, cron):
         super(CronSchedule, self).__init__()
+        self._expression = cron
         self._cron = CronTab(cron)
+        
+    def __str__(self):
+        return 'CronSchedule(%s)' % self._expression
         
     def is_overdue(self, now, last):
         delta = self._cron.next(last)
         delta = timedelta(seconds=delta)
         return last + delta <= now
     
-class MINUTELY(CronSchedule):
+class Minutely(CronSchedule):
     '''
     >>> from datetime import datetime
-    >>> schedule = MINUTELY()
+    >>> schedule = Minutely()
     >>> schedule.is_overdue(datetime(2000, 1, 1, 0, 1, 0), datetime(2000, 1, 1, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 1, 1, 0, 0, 30), datetime(2000, 1, 1, 0, 0, 0))
@@ -37,19 +45,19 @@ class MINUTELY(CronSchedule):
     '''
     
     def __init__(self):
-        super(MINUTELY, self).__init__('* * * * *')
+        super(Minutely, self).__init__('* * * * *')
         
-class HOURLY(CronSchedule):
+class Hourly(CronSchedule):
     '''
     >>> from datetime import datetime
     
-    >>> schedule = HOURLY()
+    >>> schedule = Hourly()
     >>> schedule.is_overdue(datetime(2000, 1, 1, 1, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 1, 1, 0, 30, 0), datetime(2000, 1, 1, 0, 0, 0))
     False
     
-    >>> schedule = HOURLY(30)
+    >>> schedule = Hourly(30)
     >>> schedule.is_overdue(datetime(2000, 1, 1, 1, 30, 0), datetime(2000, 1, 1, 0, 30, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 1, 1, 1, 0, 0), datetime(2000, 1, 1, 0, 30, 0))
@@ -57,19 +65,19 @@ class HOURLY(CronSchedule):
     '''
     
     def __init__(self, minute=0):
-        super(HOURLY, self).__init__('%d * * * *' % minute)
+        super(Hourly, self).__init__('%d * * * *' % minute)
         
-class DAILY(CronSchedule):
+class Daily(CronSchedule):
     '''
     >>> from datetime import datetime
     
-    >>> schedule = DAILY()
+    >>> schedule = Daily()
     >>> schedule.is_overdue(datetime(2000, 1, 2, 0, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 1, 1, 12, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
     False
     
-    >>> schedule = DAILY(12)
+    >>> schedule = Daily(12)
     >>> schedule.is_overdue(datetime(2000, 1, 2, 12, 0, 0), datetime(2000, 1, 1, 12, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 1, 1, 1, 0, 0), datetime(2000, 1, 1, 12, 0, 0))
@@ -77,19 +85,19 @@ class DAILY(CronSchedule):
     '''
     
     def __init__(self, hour=0):
-        super(DAILY, self).__init__('0 %d * * *' % hour)
+        super(Daily, self).__init__('0 %d * * *' % hour)
         
-class WEEKLY(CronSchedule):
+class Weekly(CronSchedule):
     '''
     >>> from datetime import datetime
     
-    >>> schedule = WEEKLY()
+    >>> schedule = Weekly()
     >>> schedule.is_overdue(datetime(2000, 1, 10, 0, 0, 0), datetime(2000, 1, 3, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 1, 6, 0, 0, 0), datetime(2000, 1, 3, 0, 0, 0))
     False
     
-    >>> schedule = WEEKLY(3)
+    >>> schedule = Weekly(3)
     >>> schedule.is_overdue(datetime(2000, 1, 13, 0, 0, 0), datetime(2000, 1, 6, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 1, 10, 0, 0, 0), datetime(2000, 1, 6, 0, 0, 0))
@@ -97,19 +105,19 @@ class WEEKLY(CronSchedule):
     '''
     
     def __init__(self, day=0):
-        super(WEEKLY, self).__init__('0 0 * * %d' % day)
+        super(Weekly, self).__init__('0 0 * * %d' % day)
         
-class MONTHLY(CronSchedule):
+class Monthly(CronSchedule):
     '''
     >>> from datetime import datetime
     
-    >>> schedule = MONTHLY()
+    >>> schedule = Monthly()
     >>> schedule.is_overdue(datetime(2000, 2, 1, 0, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 1, 15, 0, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
     False
     
-    >>> schedule = MONTHLY(15)
+    >>> schedule = Monthly(15)
     >>> schedule.is_overdue(datetime(2000, 2, 15, 0, 0, 0), datetime(2000, 1, 15, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 2, 1, 0, 0, 0), datetime(2000, 1, 15, 0, 0, 0))
@@ -117,19 +125,19 @@ class MONTHLY(CronSchedule):
     '''
     
     def __init__(self, day=1):
-        super(MONTHLY, self).__init__('0 0 %d * *' % day)
+        super(Monthly, self).__init__('0 0 %d * *' % day)
         
-class YEARLY(CronSchedule):
+class Yearly(CronSchedule):
     '''
     >>> from datetime import datetime
     
-    >>> schedule = YEARLY()
+    >>> schedule = Yearly()
     >>> schedule.is_overdue(datetime(2001, 1, 1, 0, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 7, 1, 0, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
     False
     
-    >>> schedule = YEARLY(7)
+    >>> schedule = Yearly(7)
     >>> schedule.is_overdue(datetime(2001, 7, 1, 0, 0, 0), datetime(2000, 7, 1, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2001, 1, 1, 0, 0, 0), datetime(2000, 7, 1, 0, 0, 0))
@@ -137,12 +145,12 @@ class YEARLY(CronSchedule):
     '''
     
     def __init__(self, month=1):
-        super(YEARLY, self).__init__('0 0 1 %d *' % month)
+        super(Yearly, self).__init__('0 0 1 %d *' % month)
         
-class MINUTES(CronSchedule):
+class Minutes(CronSchedule):
     '''
     >>> from datetime import datetime
-    >>> schedule = MINUTES(5)
+    >>> schedule = Minutes(5)
     >>> schedule.is_overdue(datetime(2000, 1, 1, 0, 5, 0), datetime(2000, 1, 1, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 1, 1, 0, 3, 0), datetime(2000, 1, 1, 0, 0, 0))
@@ -150,12 +158,12 @@ class MINUTES(CronSchedule):
     '''
     
     def __init__(self, interval):
-        super(MINUTES, self).__init__('*/%d * * * *' % interval)
+        super(Minutes, self).__init__('*/%d * * * *' % interval)
         
-class HOURS(CronSchedule):
+class Hours(CronSchedule):
     '''
     >>> from datetime import datetime
-    >>> schedule = HOURS(4)
+    >>> schedule = Hours(4)
     >>> schedule.is_overdue(datetime(2000, 1, 1, 4, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 1, 1, 3, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
@@ -163,12 +171,12 @@ class HOURS(CronSchedule):
     '''
     
     def __init__(self, interval):
-        super(HOURS, self).__init__('0 */%d * * *' % interval)
+        super(Hours, self).__init__('0 */%d * * *' % interval)
 
-class DAYS(CronSchedule):
+class Days(CronSchedule):
     '''
     >>> from datetime import datetime
-    >>> schedule = DAYS(5)
+    >>> schedule = Days(5)
     >>> schedule.is_overdue(datetime(2000, 1, 6, 0, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 1, 3, 0, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
@@ -176,12 +184,12 @@ class DAYS(CronSchedule):
     '''
     
     def __init__(self, interval):
-        super(DAYS, self).__init__('0 0 */%d * *' % interval)
+        super(Days, self).__init__('0 0 */%d * *' % interval)
         
-class MONTHS(CronSchedule):
+class Months(CronSchedule):
     '''
     >>> from datetime import datetime
-    >>> schedule = MONTHS(3)
+    >>> schedule = Months(3)
     >>> schedule.is_overdue(datetime(2000, 4, 1, 0, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
     True
     >>> schedule.is_overdue(datetime(2000, 3, 1, 0, 0, 0), datetime(2000, 1, 1, 0, 0, 0))
@@ -189,7 +197,7 @@ class MONTHS(CronSchedule):
     '''
     
     def __init__(self, interval):
-        super(MONTHS, self).__init__('0 0 1 */%d *' % interval)
+        super(Months, self).__init__('0 0 1 */%d *' % interval)
 
 if __name__ == '__main__':
     doctest.testmod()

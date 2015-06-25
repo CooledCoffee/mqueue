71f7ca37745f2d2d43f8c963c515cfaf9aa98647
CooledCoffee <CooledCoffee@gmail.com>
2015-06-25 13:25:13 +0800

    version 0.4.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name='mqueue',
-    version='0.4.1',
+    version='0.4.2',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     classifiers = [
